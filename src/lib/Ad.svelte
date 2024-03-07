5bf7edb5d9f3d4fe21b8e7efad116476dd9a4e47
Nolux <Atle.selbek@gmail.com>
2024-03-08 00:41:57 +0100

    ad changes

--- src/lib/Ad.svelte
+++ src/lib/Ad.svelte
@@ -6,9 +6,11 @@
   });
 </script>
 
-<ins
-  class="adsbygoogle"
-  style="display:inline-block;width:728px;height:90px"
-  data-ad-client="ca-pub-3153837897413513"
-  data-ad-slot="4083679616"
-></ins>
+<div class="ads-widget-container">
+  <ins
+    class="adsbygoogle"
+    style="display:inline-block;width:728px;height:90px"
+    data-ad-client="ca-pub-3153837897413513"
+    data-ad-slot="4083679616"
+  ></ins>
+</div>
