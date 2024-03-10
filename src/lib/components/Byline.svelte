3b8eae04ffe60cdad1d9f361f9919a1485443cbc
Nolux <Atle.selbek@gmail.com>
2024-03-11 00:27:29 +0100

    link change

--- src/lib/components/Byline.svelte
+++ src/lib/components/Byline.svelte
@@ -1,5 +1,5 @@
 <div class="mt-2 flex w-full justify-center text-white text-xs">
-  Atle Selbek © 2024 -<a href="https://github.com/Nolux/stratagemhero2/issues">
+  Atle Selbek © 2024 -<a href="https://github.com/Nolux/strathero2/issues">
     - Found a bug?</a
   >
 </div>
